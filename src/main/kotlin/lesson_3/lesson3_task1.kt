2d3d1c419598483e253c0ce040d7d7c970809fe4
nikulindev <etci3200@gmail.com>
2026-03-15 20:32:50 +0100

    KS-3-1 greeting in the app

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Ибрагим"
+    var greetingText = "Добрый день, $userName!"
+
+    println(greetingText)
+
+    greetingText = "Добрый вечер, $userName!"
+    println(greetingText)
+
+}
