b4602f326822a7db484ec189784a605f730d9d52
etcidev <etci3200@gmail.com>
2026-04-26 09:37:56 +0200

    KS-8-3 implemented ingredient search using find without loops

---
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val ingredients = arrayOf("Соль", "Перец", "Морковь", "Лук")
+
+    println("Напишите ингредиент, который хотите найти")
+
+    val input = readln().trim()
+
+    val foundIngredient = ingredients.find {
+        it.equals(input, ignoreCase = true)
+    }
+
+    if (foundIngredient == null) {
+        println("Такого ингредиента в рецепте нет")
+    } else {
+        println("Ингредиент $foundIngredient в рецепте есть")
+    }
+}
+
