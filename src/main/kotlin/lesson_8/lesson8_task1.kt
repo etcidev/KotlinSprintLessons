4b8e3ff8e0d34006cd3486870a48b0cf9f1ae879
etcidev <etci3200@gmail.com>
2026-04-26 17:21:44 +0200

    KS-8-1 removed function and initialized array directly

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -2,31 +2,10 @@ package org.example.lesson_8
 
 fun main() {
 
-    val dailyViews = IntArray(7)
+    val dailyViews = intArrayOf(2, 4, 8, 10, 22, 13, 11)
 
-    for (i in dailyViews.indices) {
-
-        dailyViews[i] = readViewsForDay(i + 1)
-
-    }
-
-    println(dailyViews.sum())
+    println("Всего просмотров за неделю: ${dailyViews.sum()}")
 
 }
 
-fun readViewsForDay(day: Int): Int {
 
-    while (true) {
-
-        println("Введите количество просмотров за $day день")
-        val input = readln().toIntOrNull()
-
-        if (input != null && input >= 0) {
-            return input
-        }
-
-        println("Ошибка: введите число >= 0")
-
-    }
-
-}
