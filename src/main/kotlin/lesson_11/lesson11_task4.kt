c840b35430233d5f72e95f0b80170a1f064873c7
etcidev <etci3200@gmail.com>
2026-05-06 13:07:06 +0200

    KS-11-4 update recipe models structure

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -3,19 +3,21 @@ package org.example.lesson_11
 class Category(
     val name: String,
     val description: String,
-    val picture: Int,
-
+    val imageUrl: String,
+    val recipes: List<Recipe>,
 )
 
 class Recipe(
     val name: String,
     val servings: Int,
-    val picture: Int,
+    val imageUrl: String,
     val isFavorite: Boolean = false,
     val ingredients: List<Ingredient>,
+    val method: List<String>,
 )
 
 class Ingredient(
     val name: String,
-    val amount: String,
+    val quantity: String,
+    val unitOfMeasure: String,
 )
