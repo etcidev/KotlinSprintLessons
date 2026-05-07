4951a6f4f9347d49feaa9b3a4bcc1f1728ad79aa
etcidev <etci3200@gmail.com>
2026-05-07 12:24:44 +0200

    KS-12-3 add WeatherForecast class with Kelvin to Celsius conversion

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,34 @@
+package org.example.lesson_12
+
+const val KELVIN_OFFSET = 273
+
+class WeatherForecast(
+    dailyTemperatureKelvin: Int,
+    nightTemperatureKelvin: Int,
+    isPrecipitationExpected: Boolean,
+) {
+
+    val dailyTemperature = dailyTemperatureKelvin - KELVIN_OFFSET
+    val nightTemperature = nightTemperatureKelvin - KELVIN_OFFSET
+    val isPrecipitationExpected = isPrecipitationExpected
+
+    fun printWeather() {
+        println(
+            """
+            Дневная температура: $dailyTemperature °C
+            Ночная температура: $nightTemperature °C
+            Осадки: $isPrecipitationExpected
+            """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val weatherForecast = WeatherForecast(
+        dailyTemperatureKelvin = 300,
+        nightTemperatureKelvin = 280,
+        isPrecipitationExpected = true,
+    )
+
+    weatherForecast.printWeather()
+}
